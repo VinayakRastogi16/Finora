f02e8a8a3fa9f716e274ae79bd8bca2dff425706
Vinayak Rastogi <vinayakr841@gmail.com>
2026-02-02 18:37:23 +0530

    Initial Setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import HomePage from './Landing Page/home/HomePage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <HomePage/>
   </React.StrictMode>
 );
