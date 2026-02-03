fbb5c4de0fcff06ac952e3a98b5d129aa6ee3258
Vinayak Rastogi <vinayakr841@gmail.com>
2026-02-03 20:15:23 +0530

    home-hero

---
+++ src/Landing Page/home/Awards.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Awards() {
+    return ( <>
+        <div className='container mt-5'>
+            <div className="row">
+                <div className="col-6">
+                    <img  src="media/images/largestBroker.svg" alt="" />
+
+                </div>
+                <div className="col-6">
+
+                </div>
+            </div>
+        </div>
+    </> );
+}
+
+export default Awards;
