a9cfb5ab12b3ff65aa1403f9b4a9a20fcc7d51cc
Vinayak Rastogi <vinayakr841@gmail.com>
2026-02-11 22:38:22 +0530

    Pricing changes

--- src/Landing Page/Footer.jsx
+++ src/Landing Page/Footer.jsx
@@ -2,22 +2,23 @@ import React from "react";
 
 function Footer() {
   return (
-    <div style={{ backgroundColor: "#FBFBFB", borderTop: "1px solid #EEEEEE" }}>
+    <div style={{ backgroundColor: "#FBFBFB", borderTop: "1px solid #EEEEEE", paddingTop:"3%" }}>
       <div className="container">
         <div className="row">
           <div className="col-3">
             <img
-              style={{ margin: "10% 0 5% 0", width: "50%" }}
+              style={{ margin: "10% 0 5% 0", width: "80%" }}
               src="media/images/logo.svg"
               alt="logo"
+              
             />
-            <p style={{ fontSize: "90%" }}>
-              © 2010 - 2026, Zerodha Broking Ltd.
+            <p style={{ fontSize: "x-small" }}>
+              © 2010 - 2026, Zerodha Broking <br /> Ltd.
               <br />
               All rights reserved.
             </p>
             <div
-              className="d-flex col-6"
+              className="col"
               style={{
                 fontWeight: "bold",
                 fontSize: "1.3rem",
@@ -44,7 +45,7 @@ function Footer() {
             </div>
             
             <div
-              className="d-flex col-6"
+              className="d-flex col"
               style={{
                 fontWeight: "bold",
                 fontSize: "1.3rem",
@@ -64,45 +65,45 @@ function Footer() {
           </div>
           <div className="col mt-4 mb-5 " style={{marginLeft:"5%"}}>
             <h3 className="heading">Account</h3>
-            <a href="/" className="fs-5 hover">Open demat account</a> <br/>
-            <a href="/" className="fs-5 hover">Minor demat account</a> <br/>
-            <a href="/" className="fs-5 hover">NRI demat account</a> <br/>
-            <a href="/" className="fs-5 hover">Commodity</a> <br/>
-            <a href="/" className="fs-5 hover">Dematerialisation</a> <br/>
-            <a href="/" className="fs-5 hover">Fund Transfer</a> <br/>
-            <a href="/" className="fs-5 hover">MTF</a> <br/>
-            <a href="/" className="fs-5 hover">Referral Program</a>
+            <a href="/" className="hover">Open demat account</a> <br/><br />
+            <a href="/" className="hover">Minor demat account</a> <br/><br />
+            <a href="/" className="hover">NRI demat account</a> <br/><br />
+            <a href="/" className="hover">Commodity</a> <br/><br />
+            <a href="/" className="hover">Dematerialisation</a> <br/><br />
+            <a href="/" className="hover">Fund Transfer</a> <br/><br />
+            <a href="/" className="hover">MTF</a> <br/><br />
+            <a href="/" className="hover">Referral Program</a>
           </div>
           <div className="col mt-4 ">
             <h3 className="heading">Support</h3>
-            <a href="/" className="fs-5 hover">Contact us</a> <br/>
-            <a href="/" className="fs-5 hover">Support portal</a> <br/>
-            <a href="/" className="fs-5 hover">How to file a complaint?</a> <br/>
-            <a href="/" className="fs-5 hover">Status of your complaints</a> <br/>
-            <a href="/" className="fs-5 hover">Bulletin</a> <br/>
-            <a href="/" className="fs-5 hover">Circular</a> <br/>
-            <a href="/" className="fs-5 hover">Z-Connect blog</a> <br/>
-            <a href="/" className="fs-5 hover">Downloads</a>
+            <a href="/" className="hover">Contact us</a> <br/><br />
+            <a href="/" className="hover">Support portal</a> <br/><br />
+            <a href="/" className="hover">How to file a complaint?</a> <br/><br />
+            <a href="/" className="hover">Status of your complaints</a> <br/><br />
+            <a href="/" className="hover">Bulletin</a> <br/><br />
+            <a href="/" className="hover">Circular</a> <br/><br />
+            <a href="/" className="hover">Z-Connect blog</a> <br/><br />
+            <a href="/" className="hover">Downloads</a>
           </div>
           <div className="col mt-4 ">
             <h3 className="heading">Company</h3>
-            <a href="/" className="fs-5 hover">About</a> <br/>
-            <a href="/" className="fs-5 hover">Philosophy</a> <br/>
-            <a href="/" className="fs-5 hover">Press & media</a> <br/>
-            <a href="/" className="fs-5 hover">Careers </a> <br/>
-            <a href="/" className="fs-5 hover">Zerodha Cares (CSR)</a> <br/>
-            <a href="/" className="fs-5 hover">Zerodha.tech</a> <br/>
-            <a href="/" className="fs-5 hover">Open Source</a>
+            <a href="/" className=" hover">About</a> <br/><br />
+            <a href="/" className=" hover">Philosophy</a> <br/><br />
+            <a href="/" className=" hover">Press & media</a> <br/><br />
+            <a href="/" className=" hover">Careers </a> <br/><br />
+            <a href="/" className=" hover">Zerodha Cares (CSR)</a> <br/><br />
+            <a href="/" className=" hover">Zerodha.tech</a> <br/><br />
+            <a href="/" className=" hover">Open Source</a>
           </div>
           <div className="col mt-4 ">
             <h3 className="heading">Quick Links</h3>
-            <a href="/"  className="fs-5 hover">Upcoming IPOs</a> <br/>
-            <a href="/" className="fs-5 hover">Brokerage charges</a> <br/>
-            <a href="/" className="fs-5 hover">Market holidays</a> <br/>
-            <a href="/" className="fs-5 hover ">Economic calendar </a> <br/>
-            <a href="/" className="fs-5 hover">Calculators</a>  <br/>
-            <a href="/" className="fs-5 hover">Markets</a> <br/>
-            <a href="/" className="fs-5 hover">Sectors</a> <br/>
+            <a href="/"  className="hover">Upcoming IPOs</a> <br/><br />
+            <a href="/" className="hover">Brokerage charges</a> <br/><br />
+            <a href="/" className="hover">Market holidays</a> <br/><br />
+            <a href="/" className="hover ">Economic calendar </a> <br/><br />
+            <a href="/" className="hover">Calculators</a>  <br/><br />
+            <a href="/" className="hover">Markets</a> <br/><br />
+            <a href="/" className="hover">Sectors</a> <br/>
           </div>
         </div>
         <div className="footer-text">
@@ -113,18 +114,18 @@ function Footer() {
             Address: Zerodha Broking Ltd., #153/154, 4th Cross, Dollars Colony,
             Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru -
             560078, Karnataka, India. For any complaints pertaining to
-            securities broking please write to complaints@zerodha.com, for DP
-            related to dp@zerodha.com. Please ensure you carefully read the Risk
+            securities broking please write to <span className="email">complaints@zerodha.com</span>, for DP
+            related to <span className="email">dp@zerodha.com</span>. Please ensure you carefully read the Risk
             Disclosure Document as prescribed by SEBI | ICF
             <br />
             <br />
-            Procedure to file a complaint on SEBI SCORES: Register on SCORES
+            Procedure to file a complaint on <span className="email">SEBI SCORES:</span> Register on SCORES
             portal. Mandatory details for filing complaints on SCORES: Name,
             PAN, Address, Mobile Number, E-mail ID. Benefits: Effective
             Communication, Speedy redressal of the grievances
             <br />
             <br />
-            Smart Online Dispute Resolution | Grievances Redressal Mechanism
+            <span className="email">Smart Online Dispute Resolution | Grievances Redressal Mechanism</span>
             <br />
             <br />
             Investments in securities market are subject to market risks; read
